e2aa99e6593773de2a18a46dc3a3b1b43be7cca5
arcway <arcthur.sway@gmail.com>
2010-09-01 22:20:37 +0800

    init the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Castle::Application.config.secret_token = 'ce9a528cf543f5dab011777d609cbb2e300418f1d3bd767e8278f5f9392177f896a5a4508b4820b5fee5d761f2c1a12fbcfbbea390cfef4a039b2352a30018ef'
